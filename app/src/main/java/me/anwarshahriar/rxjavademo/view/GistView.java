4fb5011c6e1d219ae4f59dde32a00eab1bbed038
anwarshahriar <anwarshahriar@gmail.com>
2017-04-16 15:32:35 +0600

    Apply MVP ui pattern

--- app/src/main/java/me/anwarshahriar/rxjavademo/view/GistView.java
+++ app/src/main/java/me/anwarshahriar/rxjavademo/view/GistView.java
@@ -1,12 +1,14 @@
-package me.anwarshahriar.rxjavademo;
+package me.anwarshahriar.rxjavademo.view;
 
 import java.util.List;
 import me.anwarshahriar.rxjavademo.model.Gist;
 
 public interface GistView {
   void showEmptyUsernameError(String message);
-  void showNoGistsStatus(String message);
+  void showNoGistsStatus();
+  void hideNoGistsStatus();
   void showLoading();
   void hideLoading();
+  String getUsername();
   void showGists(List<Gist> gists);
 }
